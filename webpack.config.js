b38decb7bd972efbceec166a84e89a7bd9d20720
ywenc <yaqing@yaqings-MBP.home>
2016-12-22 00:27:56 -0500

    Draw moving sprites

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 module.exports = {
-  entry: "./lib/snackman.js",
+  entry: "./lib/gameplay.js",
   output: {
   	filename: "./lib/bundle.js"
   },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
   resolve: {
-    extensions: ['', ',js']
+    extensions: ['', '.js']
   },
   devtool: 'source-map',
 };
